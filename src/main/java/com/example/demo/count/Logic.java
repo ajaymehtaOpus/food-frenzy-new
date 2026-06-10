59f12051305f15f560fca1643a1012aed54b9aa2
Ajay Mehta (IND_PUN <Ajay.Mehta@opustechglobal.com>
2026-06-10 17:21:33 +0530

    Add sonar-only trigger issue

--- src/main/java/com/example/demo/count/Logic.java
+++ src/main/java/com/example/demo/count/Logic.java
@@ -1,11 +1,12 @@
 // sonar remediation trigger 5
 package com.example.demo.count;
-public class Logic
-{
-private String sonarTriggerIssue = "temporary sonar trigger";
- public static double countTotal(double price,int quantity)
- {
-	 double res=price*quantity;
-	 return res;
- }
+
+public class Logic {
+
+    private String sonarTriggerIssue = "temporary sonar trigger";
+
+    public static double countTotal(double price, int quantity) {
+        double res = price * quantity;
+        return res;
+    }
 }
